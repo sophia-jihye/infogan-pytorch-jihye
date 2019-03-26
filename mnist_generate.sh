3fdb7583731c5f72985c8ae6a8cfaef246c8719c
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-26 14:54:01 +0900

    update mnist_generate.sh, checked in 88

--- mnist_generate.sh
+++ mnist_generate.sh
@@ -1,2 +1,3 @@
 echo $1
-python mnist_generate.py --load_path ./checkpoint/model_epoch100_MNIST_$1_d1c2
+echo $2
+python mnist_generate.py --anomaly_label $1 --load_path ./checkpoint/model_epoch100_MNIST_$1_d1c2 --filename $2
