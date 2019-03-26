2e1cf54a34136dd36fc3d6b8c6b80c1fefa7719b
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-26 23:24:13 +0900

    changed beta1: 0.5 -> 0.3. Testing in 88

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ params = {
     'filename' : args.filename,
     'num_epochs': 500,  # 500   # Number of epochs to train for.
     'learning_rate': 2e-4,  # Learning rate.
-    'beta1': 0.5,
+    'beta1': 0.3,	# 0.5
     'beta2': 0.999,  # 0.999
     'save_epoch': 100,  # After how many epochs to save checkpoints and generate test output.
     'dataset': 'MNIST'}  # Dataset to use. Choose from {MNIST, SVHN, CelebA, FashionMNIST}. CASE MUST MATCH EXACTLY!!!!!
