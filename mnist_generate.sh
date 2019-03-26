425f55c4fe314b3c3ef9e8f665088238ba488491
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-03-26 22:22:48 +0900

    checked train/generate/test_anomaly in 88

--- mnist_generate.sh
+++ mnist_generate.sh
@@ -1,3 +1,4 @@
 echo $1
 echo $2
-python mnist_generate.py --anomaly_label $1 --load_path ./checkpoint/model_epoch100_MNIST_$1_d1c2 --filename $2
+echo $3
+python mnist_generate.py --anomaly_label $1 --load_path ./checkpoint/model_epoch$2_MNIST_$1_d1c2 --filename $3
--- test_anomaly_88.sh
+++ test_anomaly_88.sh
@@ -1,5 +1,7 @@
-echo $1
-echo $2
-echo $3
-CUDA_VISIBLE_DEVICES=$1 python mnist_anogan.py --load_path ./checkpoint/model_final500_MNIST_$2_d1c2 --trainYn false --anomaly_label $2 --anonum 0.1 --base_score 0 --filename $3
+echo $1	#DEVICES
+echo $2 #anomaly_label
+echo $3 #epoch
+echo $4 #anonum
+echo $5 #filename
+CUDA_VISIBLE_DEVICES=$1 python mnist_anogan.py --load_path ./checkpoint/model_epoch$3_MNIST_$2_d1c2 --trainYn false --anomaly_label $2 --anonum $4 --base_score 0 --filename $5
 
