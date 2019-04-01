20ef31a6c417a515236421b8f9dd2ed14b6d26b2
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-04-01 13:25:58 +0900

    test.sh

--- test_anomaly_88.sh
+++ test_anomaly_88.sh
@@ -3,7 +3,7 @@ echo $2 #anomaly_label
 echo $3 #anonum
 echo $4 #filename
 echo $5 #epoch
-echo $6 #lambda_r
-echo $7 #lambda_dis
-CUDA_VISIBLE_DEVICES=$1 python mnist_anogan.py --load_path ./checkpoint/model_epoch$5_MNIST_$2_d1c2_beta0.5 --trainYn false --anomaly_label $2 --anonum $3 --base_score 0 --filename $4 --lambda_res $6 --lambda_disc $7 --lambda_cdis 0.0 --lambda_ccon 0.0 --sim_num 50 --dis_c_dim 10
+echo $6 #dis_c_dim
+echo $7 #num_con_c
+CUDA_VISIBLE_DEVICES=$1 python mnist_anogan.py --load_path ./checkpoint/model_epoch$5_MNIST_$2_d1c2_beta0.5 --trainYn false --anomaly_label $2 --anonum $3 --base_score 0 --filename $4 --lambda_res 1.0 --lambda_disc 0.0 --lambda_cdis 0.0 --lambda_ccon 0.0 --sim_num 50 --dis_c_dim $6 --num_con_c $7
 
