50b96ccb05112dd447361c8273cbdf8092837a51
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-04-01 12:35:05 +0900

    custome dim, num_con_c. checked 'train.sh' in 88

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 echo $1
 echo $2
-CUDA_VISIBLE_DEVICES=$1 python train.py --trainYn true --load_path ./checkpoint --anomaly_label $2
+CUDA_VISIBLE_DEVICES=$1 python train.py --trainYn true --load_path ./checkpoint --anomaly_label $2 --dis_c_dim 8 --num_con_c 3
