09e9b8fc37282845cd97ad75a5b7e5317c9a8cd7
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2019-05-23 12:15:22 +0900

    updated config.

--- mnist_generate.sh
+++ mnist_generate.sh
@@ -1,3 +1,4 @@
 echo $1	# DEVICE
 echo $2 # filename
-CUDA_VISIBLE_DEVICESES=$1 python mnist_generate.py --anomaly_label 0 --load_path ./checkpoint/model_epoch100_MNIST_0_d1c2 --filename $2
+echo $3 # model_name
+CUDA_VISIBLE_DEVICESES=$1 python mnist_generate.py --anomaly_label 0 --load_path ./checkpoint/$3 --filename $2
--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 echo $1	# DEVICE
 echo $2 # ANOMALY_LABEL
-CUDA_VISIBLE_DEVICES=$1 python train.py --trainYn true --load_path ./checkpoint --anomaly_label $2 --dis_c_dim 9 --num_con_c 3
+echo $3 #number of continuous latent variables
+CUDA_VISIBLE_DEVICES=$1 python train.py --trainYn true --load_path ./checkpoint --anomaly_label $2 --dis_c_dim 9 --num_con_c $3
